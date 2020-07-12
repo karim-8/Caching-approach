c6abac77236d271f292b93c8712abbadc292a9e0
Karim <KariimAhmed8@gmail.com>
2020-07-12 16:47:09 +0200

    Initial Commit

---
+++ Vodafone task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vodafone task
+//
+//  Created by Karem on 7/12/20.
+//  Copyright © 2020 Karem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
